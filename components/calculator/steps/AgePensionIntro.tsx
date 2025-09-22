2a32c28e5aa5d3b562c0e3f167bc8dcaf8a861c8
Thomas Yao <thomasyao15@gmail.com>
2025-09-22 11:53:00 +1000

    update pension max amount

--- components/calculator/steps/AgePensionIntro.tsx
+++ components/calculator/steps/AgePensionIntro.tsx
@@ -16,7 +16,7 @@ export default function AgePensionIntro() {
     <div className="flex flex-col items-center justify-center min-h-[calc(100vh-16rem)]">
       <TextDisplay
         title="Did you know?"
-        subtitle="The Age Pension can provide up to $43,752 per year"
+        subtitle="The Age Pension can provide up to $46,202 per year"
         content="Most Australians are eligible for some level of Age Pension. Let's check what you could receive to boost your retirement income. With AustralianSuper's lifetime income products, you could increase your Age Pension by up to 40%."
         highlight="Let's unlock your Age Pension"
       />
